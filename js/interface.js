fc4eb62c925e4b7a2c12de6de59d22b38d0c166b
M-altkbali <memealtkbali@gmail.com>
2023-07-05 19:32:51 +0200

    Edit Slider

--- js/interface.js
+++ js/interface.js
@@ -156,67 +156,67 @@
 	if (typeof $.fn.revolution !== 'undefined') {
       
       $("#rev_slider").revolution({
-        sliderType:"standard",
-        sliderLayout:"fullscreen",
-        dottedOverlay:"none",
-        delay:7000,
+        // sliderType:"standard",
+        // sliderLayout:"fullscreen",
+        // dottedOverlay:"none",
+        // delay:7000,
         navigation: {
-          keyboardNavigation:"off",
-          keyboard_direction: "horizontal",
-          onHoverStop:"off",
+        //   keyboardNavigation:"off",
+        //   keyboard_direction: "horizontal",
+        //   onHoverStop:"off",
           touch:{
-            touchenabled:"on",
-            swipe_threshold: 75,
-            swipe_min_touches: 1,
-            swipe_direction: "horizontal",
-            drag_block_vertical: false
+            // touchenabled:"on",
+            // swipe_threshold: 75,
+            // swipe_min_touches: 1,
+            // swipe_direction: "horizontal",
+            // drag_block_vertical: false
           }
         },
         viewPort: {
-          enable:true,
-          outof:"pause",
-          visible_area:"80%"
+        //   enable:true,
+        //   outof:"pause",
+        //   visible_area:"80%"
         },
-        responsiveLevels:[2048,1750,1192],
-        gridwidth:[1180,1180,980],
-        gridheight:[550],
-        lazyType:"none",
-        shadow:0,
-        spinner:"off",
-        stopLoop:"on",
-        stopAfterLoops:0,
-        shuffle:"off",
-        autoHeight:"on",
-        fullScreenAlignForce:"off",
-        fullScreenOffsetContainer: "",
-        fullScreenOffset: "",
-        disableProgressBar:"on",
-        hideThumbsOnMobile:"off",
-        hideSliderAtLimit:0,
-        hideCaptionAtLimit:0,
-        hideAllCaptionAtLilmit:0,
-        debugMode:false,
+        // responsiveLevels:[2048,1750,1192],
+        // gridwidth:[1180,1180,980],
+        // gridheight:[550],
+        // lazyType:"none",
+        // shadow:0,
+        // spinner:"off",
+        // stopLoop:"on",
+        // stopAfterLoops:0,
+        // shuffle:"off",
+        // autoHeight:"on",
+        // fullScreenAlignForce:"off",
+        // fullScreenOffsetContainer: "",
+        // fullScreenOffset: "",
+        // disableProgressBar:"on",
+        // hideThumbsOnMobile:"off",
+        // hideSliderAtLimit:0,
+        // hideCaptionAtLimit:0,
+        // hideAllCaptionAtLilmit:0,
+        // debugMode:false,
         fallbacks: {
-          simplifyAll:"off",
-          nextSlideOnWindowFocus:"off",
-          disableFocusListener:false,
+        //   simplifyAll:"off",
+        //   nextSlideOnWindowFocus:"off",
+        //   disableFocusListener:false,
         }
       });
     }
 
-  	$('.arrow-left').on('click', function(){
-   	    $("#rev_slider").revprev();
-  	});
+  	// $('.arrow-left').on('click', function(){
+   	//     $("#rev_slider").revprev();
+  	// });
   
-	$('.arrow-right').on('click', function(){
-	    $("#rev_slider").revnext();
-	});
+	// $('.arrow-right').on('click', function(){
+	//     $("#rev_slider").revnext();
+	// });
 
-	$('.slide-number .total-count').text($('#rev_slider li').size());
+	// $('.slide-number .total-count').text($('#rev_slider li').size());
 
-    $('#rev_slider').bind("revolution.slide.onchange",function (e,data) {
-	    $('.slide-number .count').text(data.slideIndex);
-    });
+    // $('#rev_slider').bind("revolution.slide.onchange",function (e,data) {
+	//     $('.slide-number .count').text(data.slideIndex);
+    // });
 
 
 
